7aabd24408d7cc1793f3146b7be373f659cd4170
Elvinas Paskauskas <ElvinasPaskauskas@gmail.com>
2020-07-29 16:16:12 +0100

    setting up react

---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import PersonIcon from "@material-ui/icons/Person";
+import ForumIcon from "@material-ui/icons/Forum";
+
+function Header() {
+  return (
+    <div>
+      <PersonIcon />
+      <ForumIcon />
+      <h2>I am a headaaa</h2>
+    </div>
+  );
+}
+
+export default Header;
